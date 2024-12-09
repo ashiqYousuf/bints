ac4df4610ab5bd47aa5f38360a35c45c9453cb9b
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-12-09 08:28:11 +0530

    Implement matrix multiplication

--- main.c
+++ main.c
@@ -5,6 +5,35 @@
 
 #include "bint.h"
 
+
+// Matrix multiplication function (big integers)
+void multiplyMatrices(bint A[][NUM_BLOCKS], size_t A_rows, size_t A_cols,
+                      bint B[][NUM_BLOCKS], size_t B_rows, size_t B_cols,
+                      bint result[][NUM_BLOCKS]) {
+    if (A_cols != B_rows) {
+        printf("Matrix multiplication dimensions mismatch\n");
+        exit(1);
+    }
+
+    // Initialize the result matrix
+    initMatrix(result, A_rows, B_cols);
+
+    // Perform the multiplication: C = A * B
+    for (size_t i = 0; i < A_rows; ++i) {
+        for (size_t j = 0; j < B_cols; ++j) {
+            // Initialize result[i][j] to zero
+            initBigInt(&result[i][j]);
+            
+            // Compute the dot product of the i-th row of A and the j-th column of B
+            for (size_t k = 0; k < A_cols; ++k) {
+                bint temp;
+                multiplyBigInt(&A[i][k], &B[k][j], &temp); // A[i][k] * B[k][j]
+                addBigInt(&result[i][j], &temp, &result[i][j]); // Add to the result
+            }
+        }
+    }
+}
+
 int main() {
     bint n1, n2, sum, mul;
 
